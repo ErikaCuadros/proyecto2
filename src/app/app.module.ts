c16b01b2f98c0dee5a2d65dc6c740f945eb70bb5
ErikaCuadros <emc.cuadros@yavirac.edu.ec>
2022-11-20 22:37:39 -0500

    xd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { UserComponent } from './pages/user/user.component';
-import { RegisterComponent } from './user/register/register.component';
 import { HeaderComponent } from './static/header/header.component';
 import { FooterComponent } from './static/footer/footer.component';
 import { NavComponent } from './static/nav/nav.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './static/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './static/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +18,12 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
     HomeComponent,
     AboutComponent,
     UserComponent,
-    RegisterComponent,
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    DashboardComponent
+    DashboardComponent,
+    BreadcrumbsComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
